fc28282f3e90fa148d5992f00619d4f492c215d0
Seth Foster <seth.foster@makerbot.com>
2015-05-18 10:57:22 -0400

    A 32-bit AVR toolchain
    So all the bitness peasants here can use it

--- avr/lib/gcc/avr/4.9.2/plugin/include/configargs.h
+++ avr/lib/gcc/avr/4.9.2/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "../configure --prefix=/home/seth/code/avr-gcc-build --target=avr --enable-languages=c,c++ --disable-nls --disable-libssp --with-dwarf2 : (reconfigured) ../configure --prefix=/home/seth/code/avr-gcc-build --target=avr --enable-languages=c,c++ --disable-nls --disable-libssp --with-dwarf2";
+static const char configuration_arguments[] = "../configure --prefix=/home/pete/gcc-avr-output --target=avr --enable-languages=c,c++ --disable-nls --disable-libssp --with-dwarf2";
 static const char thread_model[] = "single";
 
 static const struct {
